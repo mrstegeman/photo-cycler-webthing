3fa3acb813ebd789e4ceb5a39ca6e12edf1e4f19
Michael Stegeman <michael@stegeman.me>
2019-01-17 15:10:20 -0700

    Fix Python run.sh script on Debian.

--- python/run.sh
+++ python/run.sh
@@ -3,8 +3,13 @@
 dir=$(readlink -f $(dirname "$0"))
 cd "${dir}"
 
+system_option=""
+if [ -f /etc/debian_version ]; then
+    system_option="--system"
+fi
+
 if [ ! -d lib ]; then
-    pip3 install --target lib -r requirements.txt --prefix ""
+    pip3 install $system_option --install-option="--prefix=" --target lib -r requirements.txt
 fi
 
 PYTHONPATH=./lib python3 photo-cycler-webthing.py "${dir}/../photos" "${dir}/../static"
